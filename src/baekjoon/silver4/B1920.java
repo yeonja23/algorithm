af72f2aa029a4f4cd1756b9feb93414b9d19e0b8
Park, Yeonji <yeonpyj23@gmail.com>
2026-03-15 17:55:11 +0900

    refactor: rename class to B1920

--- src/baekjoon/silver4/B1920.java
+++ src/baekjoon/silver4/B1920.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-public class B11478 {
+public class B1920 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();

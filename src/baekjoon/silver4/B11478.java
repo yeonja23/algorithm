67593b2ae1312f8aa45ec3864c8cddb8c36df48a
Park, Yeonji <yeonpyj23@gmail.com>
2026-03-15 17:52:25 +0900

    fix: resolve time limit exceeded in BOJ 11478

--- src/baekjoon/silver4/B11478.java
+++ src/baekjoon/silver4/B11478.java
@@ -3,48 +3,36 @@ package baekjoon.silver4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class B11478 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        int[] numbers = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        Set<String> set1 = new HashSet<>();
+
         for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+            set1.add(st.nextToken());
         }
 
         int M = Integer.parseInt(br.readLine());
-        int[] targets = new int[M];
         st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            targets[i] = Integer.parseInt(st.nextToken());
-        }
-
-        boolean flag = false;
-
-        StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (numbers[j] == targets[i]) {
-                    flag = true;
-                }
-            }
-
-            if (flag) {
+            if (set1.contains(st.nextToken())) {
                 sb.append("1\n");
             } else {
                 sb.append("0\n");
             }
-
-            flag = false;
         }
 
-        System.out.printf(sb.toString());
+        System.out.println(sb.toString());
     }
 }

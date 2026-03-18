0a1c7ab933d5f9b58628e6d7a8da65b055a45c68
Park, Yeonji <yeonpyj23@gmail.com>
2026-03-18 23:14:50 +0900

    fix: resolve time limit exceeded in BOJ 10816

--- src/baekjoon/silver4/B10816/B10816.java
+++ src/baekjoon/silver4/B10816/B10816.java
@@ -3,7 +3,8 @@ package baekjoon.silver4.B10816;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class B10816 {
@@ -11,29 +12,22 @@ public class B10816 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        ArrayList<Integer> list = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
+        Map<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-           list.add(Integer.parseInt(st.nextToken()));
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         int M = Integer.parseInt(br.readLine());
+
         st = new StringTokenizer(br.readLine());
-
-        int count = 0;
-
         for (int i = 0; i < M; i++) {
-            int n = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < N; j++) {
-               if (n == list.get(j)) {
-                    count++;
-               }
-            }
-            sb.append(count + " ");
-            count = 0;
+            int target = Integer.parseInt(st.nextToken());
+
+            sb.append(map.getOrDefault(target, 0)).append(" ");
         }
 
         System.out.println(sb);

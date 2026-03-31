3c3f5b33c6a65ddf5652aaaf0ac911d4baa14c85
Park, Yeonji <yeonpyj23@gmail.com>
2026-03-31 09:43:16 +0900

    refactor: resolve B14501.java

--- src/baekjoon/silver3/B14501/B14501.java
+++ src/baekjoon/silver3/B14501/B14501.java
@@ -4,46 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class B14501 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        int[] T = new int[N];
-        int[] P = new int[N];
-
+        int[] time = new int[N];
+        int[] money = new int[N];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            T[i] = Integer.parseInt(st.nextToken());
-            P[i] = Integer.parseInt(st.nextToken());
+            time[i] = Integer.parseInt(st.nextToken());
+            money[i] = Integer.parseInt(st.nextToken());
         }
 
-        TreeSet<Integer> set = getIntegers();
-        int sum = 0;
-        int temp;
-
-        for (int i = 0; i < N; i++) { // N: 7
-            int days = T[i]; // days: 3
-            temp = N - i - 1; // temp: 6
-            sum += P[i]; // sum: 10
-            while (temp > 0) { // 1
-                sum += P[i + days]; // sum: 20
-                temp = N - days; // 4
-                days = P[i + days]; // 여기서 막힘,, for문으로
+        int[] dp = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            if (i + time[i] <= N) {
+                dp[i + time[i]] = Math.max(dp[i + time[i]], dp[i] + money[i]);
             }
-            set.add(sum);
-            sum = 0;
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
         }
-
-        sb.append(set.last());
-        System.out.println(sb);
-    }
-
-    private static TreeSet<Integer> getIntegers() {
-        TreeSet<Integer> set = new TreeSet<>();
-        return set;
+        System.out.println(dp[N]);
     }
 }

87ed301ea9f91ac51c225907eb806f5cf81b17d2
Park, Yeonji <yeonpyj23@gmail.com>
2026-03-31 10:24:55 +0900

    feat: add P42626.java

--- src/programmers/level2/P42626/P42626.java
+++ src/programmers/level2/P42626/P42626.java
@@ -1,30 +1,40 @@
 package programmers.level2.P42626;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.PriorityQueue;
 
 public class P42626 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static public int solution(int[] scoville, int K) {
 
-        int N = Integer.parseInt(br.readLine());
-        int[] time = new int[N];
-        int[] money = new int[N];
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            time[i] = Integer.parseInt(st.nextToken());
-            money[i] = Integer.parseInt(st.nextToken());
+        int answer = 0;
+
+        PriorityQueue<Integer> pQ = new PriorityQueue<>();
+        for (int i = 0; i < scoville.length; i++) {
+            pQ.add(scoville[i]);
         }
 
-        int[] dp = new int[N + 1];
-        for (int i = 0; i < N; i++) {
-            if (i + time[i] <= N) {
-                dp[i + time[i]] = Math.max(dp[i + time[i]], dp[i] + money[i]);
+        while (pQ.peek() < K) {
+            if (pQ.size() == 1) {
+                answer = -1;
+                return answer;
             }
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+            int min = pQ.poll();
+            int minNext = pQ.poll();
+
+            int newFood = min + (minNext * 2);
+
+            pQ.add(newFood);
+
+            answer++;
         }
-        System.out.println(dp[N]);
+
+        System.out.println(pQ);
+        System.out.println(answer);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] scoville = { 1, 2, 3, 9, 10, 12 };
+        solution(scoville, 7);
     }
 }

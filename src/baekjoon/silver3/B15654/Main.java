c04191c7dbae5329050b46bb20f694295a260e69
Park, Yeonji <yeonpyj23@gmail.com>
2026-06-07 18:44:24 +0900

    add B15654

---
+++ src/baekjoon/silver3/B15654/Main.java
@@ -0,0 +1,53 @@
+package baekjoon.silver3.B15654;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+    static int n, m;
+    static boolean[] visited;
+    static int[] ans;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        ans = new int[n];
+        visited = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0);
+        System.out.print(sb.toString());
+    }
+
+    public static void dfs(int depth) {
+        if (depth == m){
+            for (int i = 0; i < m; i++){
+                sb.append(ans[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++){
+            if (!visited[i]){
+                visited[i] = true;
+                ans[depth] = arr[i];
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
